712bdb9c5a6d6c49431d894e701e7ad8d654fe4c
shil-mil <shyshy122303@gmail.com>
2025-03-09 22:39:57 +0800

    Add script to fetch weather data from API

---
+++ script.js
@@ -0,0 +1,22 @@
+const api_key = "94676e7ece46fc69656ea62a8953cc22";
+const api_url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBar = document.querySelector(".search-bar input")
+const searchBtn = document.querySelector(".search-bar button")
+
+
+async function getWeather(city) {
+    const response = await fetch(api_url + city + `&appid=${api_key}`);
+    var data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+}
+
+getWeather("Davao"); //displays Davao city weather first
+
+searchBtn.addEventListener("click", ()=>{
+    getWeather(searchBar.value);
+});
