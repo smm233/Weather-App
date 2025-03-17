e84aa89a71f6c645cc9c31ada4b7a9907b1e6944
shil-mil <shyshy122303@gmail.com>
2025-03-17 18:30:32 +0800

    Add snow else if condition for weather icon src

--- script.js
+++ script.js
@@ -22,8 +22,9 @@ async function getWeather(city) {
             weatherIcon.src = "images/drizzle.png";
         } else if(data.weather[0].main == "Mist") {
             weatherIcon.src = "images/mist.png";
+        } else if(data.weather[0].main == "Snow") {
+            weatherIcon.src = "images/snow.png";
         }
-    
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
         
