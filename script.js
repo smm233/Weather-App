fe7ff553b473d5306d03401f28a44f0b01f7710a
shil-mil <shyshy122303@gmail.com>
2025-03-17 18:43:52 +0800

    Fetch and display weather description

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ async function getWeather(city) {
     if(response.status == 404) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
+        document.querySelector(".description").style.display = "none";
     } else {
         if(data.weather[0].main == "Clouds") {
             weatherIcon.src = "images/clouds.png";
@@ -27,7 +28,7 @@ async function getWeather(city) {
         }
         document.querySelector(".weather").style.display = "block";
         document.querySelector(".error").style.display = "none";
-        
+        document.querySelector(".description").innerHTML = data.weather[0].description.toUpperCase();
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
